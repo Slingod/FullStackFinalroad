ac49aece6e3a1a2f204a4c5b18ebb18a492fb3d4
slingod <slingo.drisca1@gmail.com>
2025-03-17 07:22:57 +0100

    Add road

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,24 @@
 Rails.application.routes.draw do
+  # Home Page
   root "pages#home"
 
+  # Page about and contact
   get "/about", to: "pages#about"
-  get "/contact", to: "pages#contact"
+  get "/contact", to: "contacts#new", as: :contact
+  post "/contact", to: "contacts#create"
 
+  # Devise roads for users
   devise_for :users
 
+  devise_scope :user do
+    get "signup", to: "devise/registrations#new", as: :signup
+    get "login", to: "devise/sessions#new", as: :login
+  end
+
+  # User profile
   resources :users, only: [:show]
+
+  # registration or unregistration to an event
   resources :events do
     member do
       post "register"
@@ -14,10 +26,26 @@ Rails.application.routes.draw do
     end
   end
 
+  # Picture
+  resources :pictures, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+
+  # Page Shop
+  get "shop", to: "home#shop", as: :shop
+
+  # Admin space
   namespace :admin do
-    get "dashboard", to: "dashboard#index"
+    root to: "dashboard#index"
+    resources :users
   end
 
+  # Processus for payment
+  scope '/checkout' do
+    post 'create', to: 'checkout#create', as: 'checkout_create'
+    get 'success', to: 'checkout#success', as: 'checkout_success'
+    get 'cancel', to: 'checkout#cancel', as: 'checkout_cancel'
+  end
+
+  # Page for donations
   get "/donate", to: "payments#new"
 
   get "up" => "rails/health#show", as: :rails_health_check
