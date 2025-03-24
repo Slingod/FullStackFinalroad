2e9610aba169a4616a4728271ac7f57032e0b2e9
slingod <slingo.drisca1@gmail.com>
2025-03-24 13:16:06 +0100

    Refactor user role management and enhance user controller actions for better authorization handling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,30 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   before_save :prevent_admin_change
 
-  # Association for event participation
   has_many :event_users
   has_many :events, through: :event_users
 
-  # Ensure age is a positive number or nil
   validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
 
-  # Default values for visibility settings
   after_initialize :set_default_visibility
 
   def admin?
-    self.admin
+    role == "admin"
   end
 
   def super_admin?
-    self.super_admin
+    role == "super_admin"
   end
 
+  def can_manage?(other_user)
+    return false if other_user.super_admin? && !self.super_admin? # an Admin cannot manage a Super Admin
+    return false if other_user.admin? && !self.super_admin? # an Admin cannot manage another Admin
+    true # if the other user is a regular user or if the current user is a super admin
+  end
+
+  # add a method to check if the user is over 18
   def over_18?
     age.present? && age >= 18
   end
@@ -37,11 +39,11 @@ class User < ApplicationRecord
   end
 
   def prevent_admin_change
-    return unless self.admin_changed?
+    return unless self.role_changed?
 
     current_user = self.class.find_by(id: self[:current_user_id]) if self[:current_user_id]
-    if current_user && !current_user.admin?
-      errors.add(:admin, "cannot be changed like this")
+    if current_user && !current_user.super_admin? && self.super_admin?
+      errors.add(:role, "cannot be changed by a non-superadmin")
       throw(:abort)
     end
   end
